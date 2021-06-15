91a167a9b3f4fa4115b7f03345849ad819e4a396
xiongster94 <xiongster94@users.noreply.github.com>
2021-06-15 11:54:52 -0500

    completed 3 and 4

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,21 +1,29 @@
 -- Tasks
 
---1. Get all customers and their addresses.
+-- 1. Get all customers and their addresses.
 SELECT "customers".first_name, "customers".last_name, "addresses" FROM "addresses"
-JOIN "customers" ON "customers".id = "addresses".customer_id
-;
---2. Get all orders and their line items (orders, quantity and product).
+JOIN "customers" ON "customers".id = "addresses".customer_id;
+
+-- 2. Get all orders and their line items (orders, quantity and product).
 SELECT "orders", "line_items".quantity, "products".description FROM "orders"
 JOIN "line_items" ON "line_items".order_id = "orders".id
-JOIN "products" ON "products".id = "line_items".product_id
-;
+JOIN "products" ON "products".id = "line_items".product_id;
 
---3. Which warehouses have cheetos?
---4. Which warehouses have diet pepsi?
---5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
---6. How many customers do we have?
---7. How many products do we carry?
---8. What is the total available on-hand quantity of diet pepsi?
+-- 3. Which warehouses have cheetos?
+SELECT "warehouse".warehouse FROM "warehouse"
+JOIN "warehouse_product" ON "warehouse_product".warehouse_id = "warehouse".id
+JOIN "products" ON "products".id = "warehouse_product".product_id
+WHERE "products".id = 5;
+
+-- 4. Which warehouses have diet pepsi?
+SELECT "warehouse".warehouse FROM "warehouse"
+JOIN "warehouse_product" ON "warehouse_product".warehouse_id = "warehouse".id
+JOIN "products" ON "products".id = "warehouse_product".product_id
+WHERE "products".id = 6;
+-- 5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
+-- 6. How many customers do we have?
+-- 7. How many products do we carry?
+-- 8. What is the total available on-hand quantity of diet pepsi?
 
 
 
